c3e216ac0814fe48d07aa6e23810b61318937c3c
Danny <48557943+P00PF4C3@users.noreply.github.com>
2019-05-06 13:26:52 +0200

    f
    f

--- Website/Index.php
+++ Website/Index.php
@@ -10,111 +10,113 @@
   <?php include_once 'aanroepingen/RubNav.php'; ?>
 </aside>
 
-<h1 class="HomepaginaKopjes">Aangeraden producten </h1>
-<div class="HomepaginaContainer">
-<article  >
-    <h3>Viking fiets</h3>
-    <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
-    <p>Huidige prijs:</p>
-    <h2>24,99.-</h2>
-    <p>Locatie: Arnhem</p>
-</article>
+<div class="homepaginaContainer">
 
-<article >
-    <h3>Viking fiets</h3>
-    <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
-    <p>Huidige prijs:</p>
-    <h2>24,99.-</h2>
-    <p>Locatie: Arnhem</p>
-</article>
+    <h1 class="HomepaginaKopjes">Aangeraden producten </h1>
+    <div class="homepaginaArtikelen">
+    <article>
+        <h3>Viking fiets</h3>
+        <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
+        <p>Huidige prijs:</p>
+        <h2>24,99.-</h2>
+        <p>Locatie: Arnhem</p>
+    </article>
 
-<article >
-    <h3>Viking fiets</h3>
-    <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
-    <p>Huidige prijs:</p>
-    <h2>24,99.-</h2>
-    <p>Locatie: Arnhem</p>
-</article>
+    <article >
+        <h3>Viking fiets</h3>
+        <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
+        <p>Huidige prijs:</p>
+        <h2>24,99.-</h2>
+        <p>Locatie: Arnhem</p>
+    </article>
 
-<article >
-    <h3>Viking fiets</h3>
-    <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
-    <p>Huidige prijs:</p>
-    <h2>24,99.-</h2>
-    <p>Locatie: Arnhem</p>
-</article>
+    <article >
+        <h3>Viking fiets</h3>
+        <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
+        <p>Huidige prijs:</p>
+        <h2>24,99.-</h2>
+        <p>Locatie: Arnhem</p>
+    </article>
+
+    <article >
+        <h3>Viking fiets</h3>
+        <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
+        <p>Huidige prijs:</p>
+        <h2>24,99.-</h2>
+        <p>Locatie: Arnhem</p>
+    </article>
+    </div>
+    <h1 class="HomepaginaKopjes">Recent bekeken </h1>
+    <div class="homepaginaArtikelen">
+    <article >
+        <h3>Viking fiets</h3>
+        <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
+        <p>Huidige prijs:</p>
+        <h2>24,99.-</h2>
+        <p>Locatie: Arnhem</p>
+    </article>
+
+    <article >
+        <h3>Viking fiets</h3>
+        <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
+        <p>Huidige prijs:</p>
+        <h2>24,99.-</h2>
+        <p>Locatie: Arnhem</p>
+    </article>
+
+    <article >
+        <h3>Viking fiets</h3>
+        <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
+        <p>Huidige prijs:</p>
+        <h2>24,99.-</h2>
+        <p>Locatie: Arnhem</p>
+    </article>
+
+    <article >
+        <h3>Viking fiets</h3>
+        <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
+        <p>Huidige prijs:</p>
+        <h2>24,99.-</h2>
+        <p>Locatie: Arnhem</p>
+    </article>
+
+    </div>
+    <h1 class="HomepaginaKopjes"> Uw biedingen </h1>
+    <div class="homepaginaArtikelen">
+    <article >
+        <h3>Viking fiets</h3>
+        <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
+        <p>Huidige prijs:</p>
+        <h2>24,99.-</h2>
+        <p>Locatie: Arnhem</p>
+    </article>
+
+    <article >
+        <h3>Viking fiets</h3>
+        <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
+        <p>Huidige prijs:</p>
+        <h2>24,99.-</h2>
+        <p>Locatie: Arnhem</p>
+    </article>
+
+    <article >
+        <h3>Viking fiets</h3>
+        <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
+        <p>Huidige prijs:</p>
+        <h2>24,99.-</h2>
+        <p>Locatie: Arnhem</p>
+    </article>
+
+    <article >
+        <h3>Viking fiets</h3>
+        <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
+        <p>Huidige prijs:</p>
+        <h2>24,99.-</h2>
+        <p>Locatie: Arnhem</p>
+    </article>
+
+    </div>
 </div>
-<h1 class="HomepaginaKopjes">Recent bekeken </h1>
-<div class="HomepaginaContainer">
-<article >
-    <h3>Viking fiets</h3>
-    <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
-    <p>Huidige prijs:</p>
-    <h2>24,99.-</h2>
-    <p>Locatie: Arnhem</p>
-</article>
-
-<article >
-    <h3>Viking fiets</h3>
-    <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
-    <p>Huidige prijs:</p>
-    <h2>24,99.-</h2>
-    <p>Locatie: Arnhem</p>
-</article>
-
-<article >
-    <h3>Viking fiets</h3>
-    <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
-    <p>Huidige prijs:</p>
-    <h2>24,99.-</h2>
-    <p>Locatie: Arnhem</p>
-</article>
-
-<article >
-    <h3>Viking fiets</h3>
-    <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
-    <p>Huidige prijs:</p>
-    <h2>24,99.-</h2>
-    <p>Locatie: Arnhem</p>
-</article>
-
-</div>
-<h1 class="HomepaginaKopjes"> uw biedingen </h1>
-<div class="HomepaginaContainer">
-<article >
-    <h3>Viking fiets</h3>
-    <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
-    <p>Huidige prijs:</p>
-    <h2>24,99.-</h2>
-    <p>Locatie: Arnhem</p>
-</article>
-
-<article >
-    <h3>Viking fiets</h3>
-    <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
-    <p>Huidige prijs:</p>
-    <h2>24,99.-</h2>
-    <p>Locatie: Arnhem</p>
-</article>
-
-<article >
-    <h3>Viking fiets</h3>
-    <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
-    <p>Huidige prijs:</p>
-    <h2>24,99.-</h2>
-    <p>Locatie: Arnhem</p>
-</article>
-
-<article >
-    <h3>Viking fiets</h3>
-    <a href="#" ><img src="images/fiets.JPG" alt="filmje1"/></a>
-    <p>Huidige prijs:</p>
-    <h2>24,99.-</h2>
-    <p>Locatie: Arnhem</p>
-</article>
-
-</div>
-
 
 
 
