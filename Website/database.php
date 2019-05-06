2a301123b32297b88fe978c62365dd0eecfb94b8
memo043 <mehmet.batal@hotmail.com>
2019-05-06 09:37:10 +0200

    Update database.php

--- Website/database.php
+++ Website/database.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: memoo
- * Date: 7-1-2019
- * Time: 15:56
- */
+
 $hostname = "(local)"; //Naam van de Server
 $dbname = "EenmaalAndermaal"; //Naam van de DB
 $username = "sa";      //Inlognaam
