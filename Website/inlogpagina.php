3681e78153773d57501eefd7355b97faa2c8d527
memo043 <mehmet.batal@hotmail.com>
2019-05-07 10:32:17 +0200

    Fix Loginpagina en Index
    - Footer in loginpagina op juiste locatie
    - navigatie van rubrieken heeft geen bottom margin van 100% meer.

--- Website/inlogpagina.php
+++ Website/inlogpagina.php
@@ -7,84 +7,23 @@
   <?php include_once 'aanroepingen/RubNav.php'; ?>
 </aside>
 
-<form>
-        <div class="inlogpaginaContainer">
+
+
+<form class="inlogpaginaContainer">
+        <div >
         <input type="text" placeholder="Voer Gebruikersnaam in" name="email" required>
         <input type="password" placeholder="Voer Wachtwoord in" name="psw" required>
          <hr>
         <button type="submit" class="inlogbutton">Log in</button>
+        <input type="button" class="inlogbutton" onclick="window.location.href = 'index.php';" value="Wachtwoord vergeten?"/>
+        <p>Als je nog geen account voor EenmaalAndermaal hebt maak dan <a href="index.php">hier</a> een account</p>
+        </div>
+ 
  </form>
- <input type="button" class="inlogbutton" onclick="window.location.href = 'index.php';" value="Wachtwoord vergeten?"/>
- <p>Als je nog geen account voor EenmaalAndermaal hebt maak dan <a href="index.php">hier</a> een account</p>
- </div>
+ 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php
+ <?php
   include_once 'aanroepingen/footer.html';
 ?>
