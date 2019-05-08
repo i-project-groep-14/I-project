1a1dd2c6a45836009e4a90627d96d05a6f1e20cf
Danny <48557943+P00PF4C3@users.noreply.github.com>
2019-05-08 12:45:02 +0200

    Revert "Merge branch 'master' of https://github.com/i-project-groep-14/I-project"
    This reverts commit e957170c747eadebf788467cb5337ebcbe18f048, reversing
    changes made to 7572dce206cc026686f9989d06ee21a08e4754da.

--- Website/Index.php
+++ Website/Index.php
@@ -1,7 +1,9 @@
 <?php
   require_once 'aanroepingen/connectie.php';
   include_once 'aanroepingen/header.php';
-  include_once 'timerfunctie.php';
+
+ 
+
 ?>
 
 
@@ -16,7 +18,13 @@
 </aside>
 
 
+
+
 <?php include_once "HomePageProducten.php" ?>
+
+
+
+
 
 
 <?php
--- Website/aanroepingen/RubNav.php
+++ Website/aanroepingen/RubNav.php
@@ -7,33 +7,33 @@
 			<a href="#">Antiek en Kunst</a>
 			<div >
 				<div class="nav-column">
-					<h3>Kasten</h3>
+					<h3>Home</h3>
 					<ul>
-						<li><a href="#">Kledingkast</a></li>
-						<li><a href="#">Dressoir</a></li>
-						<li><a href="#">Nachtkast</a></li>
-						<li><a href="#">Ladekast</a></li>
-						<li><a href="#">Vitrinekast</a></li>
-						<li><a href="#">Boekenkast</a></li>
-						<li><a href="#">Eikenhout</a></li>
-						<li><a href="#">Beukenhout</a></li>
+						<li><a href="#">Pampers Diapers</a></li>
+						<li><a href="#">Huggies Diapers</a></li>
+						<li><a href="#">Seventh Generation</a></li>
+						<li><a href="#">Diapers</a></li>
+						<li><a href="#">Derbies</a></li>
+						<li><a href="#">Driving shoes</a></li>
+						<li><a href="#">Espadrilles</a></li>
+						<li><a href="#">Loafers</a></li>
 						
 					</ul>
 				</div>
 
 				<div class="nav-column">
-					<h3>Schilderijen</h3>
+					<h3>Home1</h3>
 					<ul>
-						<li><a href="#">Abstracten Schilderijen</a></li>
-						<li><a href="#">Aquarel</a></li>
-						<li><a href="#">Landschap Schilderijen</a></li>
+						<li><a href="#">Driving shoes</a></li>
+						<li><a href="#">Espadrilles</a></li>
+						<li><a href="#">Loafers</a></li>
 					</ul>
-<br>
-					<h3>Kunst</h3>
+
+					<h3>Home2</h3>
 					<ul>
-						<li><a href="#">Beelden</a></li>
-						<li><a href="#">Klokken</a></li>
-						<li><a href="#">Knikkers</a></li>
+						<li><a href="#">Driving shoes</a></li>
+						<li><a href="#">Espadrilles</a></li>
+						<li><a href="#">Loafers</a></li>
 					</ul>
 				</div>
 
@@ -52,7 +52,7 @@
 				</div>
 
 				<div class="nav-column">
-					<h3 class="orange">Gerelateerde Categorieën</h3>
+					<h3 class="orange">Related Categories</h3>
 					<ul>
 						<li><a href="#">Pampers Diapers</a></li>
 						<li><a href="#">Huggies Diapers</a></li>
