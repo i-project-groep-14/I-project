997ac8820e99ecb19413f641faf3792177d9dbcb
Danny <48557943+P00PF4C3@users.noreply.github.com>
2019-05-08 13:57:25 +0200

    FIX INLOGGEN/REGISTREREN

--- Website/registratie_persoonsgegevens.php
+++ Website/registratie_persoonsgegevens.php
@@ -69,55 +69,78 @@
 
         <h2 class="HomepaginaKopjes">Registreren</h2>
         <div class="body-tekst">
-        <p>
-            Dit is de tweede stap van het registreren. Vul a.u.b al uw gevraagde gegevens hieronder in. 
-        </p>
-          <form action="registratie_persoonsgegevens.php" method="post" >
-            Gebruikersnaam:
-            <input type="text" placeholder="Gebruikersnaam" name="gebruikersnaam">
-            Voornaam & Achternaam:<br>
-            <input  type="text" placeholder="Voornaam" name="voornaam" class="small-input">
-            <input type="text" placeholder="Achternaam" naam="achternaam" class="small-input" >
-            Adres & Toevoeging Adres (Optioneel): 
+          <p>
+            Dit is de tweede stap van het registreren. Vul a.u.b uw persoonlijkegegevens hieronder in. 
+          </p>
+
+          <form action="" method="post" >
+          <label>Gebruikersnaam:</label>
+            <input type="text" placeholder="Gebruikersnaam" name="gebruikersnaam" required>
+
+            <label>Voornaam:</label>
+            <input  type="text" placeholder="Voornaam" name="voornaam" required>
+
+            <label>Achternaam:</label>
+            <input type="text" placeholder="Achternaam" name="achternaam" required>
+
+            <label>Adres:</label>
+            <input type="text" placeholder="Adres" name="adres" required>
+
+            <label>Toevoeging Adres (Optioneel): </label>
             <div class="tooltip">Meer informatie?
-              <span class="tooltiptext">Een toevoeging bij het adres is een extra adres regel voor mensen die buiten Nederland wonen.</span>
-            </div><br>
-            <input type="text" placeholder="Adres" name="adres" class="small-input" > 
-            <input type="text" placeholder="Toevoeging adres" name="oAdres" class="small-input" >
-            Postcode & Plaatsnaam:<br>
-            <input type="text" placeholder="Postcode" name="postcode" class="small-input">
-            <input type="text" placeholder="Plaats" name="plaats" class="small-input" id="plaatsnaam">        
-            Landsnaam:
-            <input type="text" placeholder="Land" name="land">
-            Telefoonnr & Telefoonnr 2 (Optioneel): <br>
-            <input type="tel" placeholder="Telefoonnr" name="telnr1" class="small-input">
-            <input type="tel" placeholder="Telefoonnr" name="telnr2" class="small-input">
-            Geboortedatum:
-            <input type="date" name="geboortedatum">
-            Wachtwoord:
-            <input type="text" placeholder="Wachtwoord" name="wachtwoord">
-            Bevestig Wachtwoord:
-            <input type="text" placeholder="Bevestig wachtwoord" name="bWachtwoord">
-            Wordt dit een verkopersaccount?<br>
+              <span class="tooltiptext">Dit is een extra adres regel voor mensen die buiten Nederland wonen.</span>
+            </div>
+            <input type="text" placeholder="Tweede adres" name="oAdres">
+
+            <label>Postcode:</label>
+            <input type="text" placeholder="Postcode" name="postcode" required>
+
+            <label>Plaatsnaam:</label>
+            <input type="text" placeholder="Plaats" name="plaats" required>
+
+            <label>Landsnaam:</label>
+            <input type="text" placeholder="Land" name="land" required>
+
+            <label>Telefoonnr:</label>
+            <input type="tel" placeholder="Telefoonnr" name="telnr1" required>
+
+            <label>Telefoonnr 2 (Optioneel):</label>
+            <input type="tel" placeholder="Telefoonnr" name="telnr2">
+
+            <label>Geboortedatum:</label>
+            <input type="date" name="geboortedatum" required>
+
+            <label>Wachtwoord:</label>
+            <input type="text" placeholder="Wachtwoord" name="wachtwoord" required>
+
+            <label>Bevestig Wachtwoord:</label>
+            <input type="text" placeholder="Bevestig wachtwoord" name="bWachtwoord" required>
+            
+            <label>Wilt u spullen verkopen?</label>
             <label class="label-next side-label" for="wel">Wel</label>
-            <input type="radio" name="eenVerkoper" id="wel"> 
+            <input type="radio" name="eenVerkoper" value="3" id="wel"> 
             <label for="niet">Niet</label>
-            <input type="radio" name="eenVerkoper" id="niet"> <br>
+            <input type="radio" name="eenVerkoper" value="2" id="niet"> <br>
             <div class="wel-verkopergegevens">
               <label for="verkoopgegevens-rekeningnr">Rekeningnummer:</label>
               <input type="text" name="verkoopgegevens-rekeningnr" placeholder="Rekeningnummer">
+
               <label for="verkoopgegevens-bank">Bank:</label>
               <input type="text" name="verkoopgegevens-bank" placeholder="Bank">
+
               <label for="controle-creditcard" class="label-next side-label">Creditcard:</label>
               <input type="radio" name="controle" id="controle-creditcard">
+
               <label for="controle-post">Post:</label>
               <input type="radio" name="controle" id="controle-post">
+
               <div class="controle-creditcard-gegevens">
                 <label for="creditcard-gegevens">Creditcardnummer</label>
                 <input type="text" name="creditcardnummer" id="creditcard-gegevens" placeholder="Creditcardnummer">
               </div>
-            </div> 
-            Dit kan na een normaal account, nog altijd een verkopersaccount worden.<br><br>
+            </div>
+            <p>Mocht u nu nog geen verkoper zijn, kunt u dit altijd later aanpassen.<p><br><br>
+            
             <input class="button" type="submit" name="verzenden_pers" value="Verzenden">
         
           </form>
--- Website/registratie_vraag.php
+++ Website/registratie_vraag.php
@@ -34,30 +34,6 @@
 
     <h2 class="HomepaginaKopjes">Veiligheidsvraag</h2>
     <div class="body-tekst">
-        <p>
-            Dit is de derde stap van het registreren. 
-            Hierin wordt naar een veiligheidsvraag gevraagd die wordt gevraagd als de gebruiker het wachtwoord is vergeten.
-            Kies hieronder uit welke vraag en geef een antwoordt. Dit antwoordt is nodig om uw wachtwoord te herstellen. 
-        </p>
-        <form action="registratie_vraag.php" methode="post">
-            <p>Kies één veiligeheidsvraag.</p>
-            <select>
-                <option>Vragen</option>
-                <option>Vraag 1</option>
-                <option>Vraag 2</option>
-                <option>Vraag 3</option>
-                <option>Vraag 4</option>
-                <option>Vraag 5</option>
-            </select>
-            <br><br>
-            <label>Vul het antwoordt in a.u.b. 
-            <input type="text" name="vraag_antwoord">
-            </label>
-            <input class="button" type="submit" value="Verzenden" name="verzenden_vraag">
-            <button class="button btn-pos-right" onclick="window.location.href = 'registratie_persoonsgegevens.php';">Terug</button>
-        </form>
-        
-</div>
       <p>
         Dit is de derde stap van het registreren. 
         Hierin wordt naar een veiligheidsvraag gevraagd die wordt gevraagd als de gebruiker het wachtwoord is vergeten.
@@ -84,6 +60,7 @@
         <br>
 
         <input class="button" type="submit" value="Verzenden" name="register">
+        <button class="button btn-pos-right" onclick="window.location.href = 'registratie_persoonsgegevens.php';">Terug</button>
       </form>
     </div>
 
