00f3f43cd7a29492af195883e5f0d32f6c8700f4
HendrikOtten28 <44369979+HendrikOtten28@users.noreply.github.com>
2019-05-09 09:46:57 +0200

    f

---
+++ Website/overons.php
@@ -0,0 +1,10 @@
+<?php
+  require_once 'aanroepingen/connectie.php';
+  include_once 'aanroepingen/header.php';
+?>
+
+<aside  class="NavRubriekAside">
+  <?php include_once 'aanroepingen/RubNav.php'; 
+        include_once 'aanroepingen/RubNavMobiel.php'
+?>
+</aside>
