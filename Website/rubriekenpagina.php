a33c2ffac52c7809aa5d163c485b0d61b1ccaa3e
HendrikOtten28 <44369979+HendrikOtten28@users.noreply.github.com>
2019-05-10 10:08:12 +0200

    rubrieken lelijker gemaakt

--- Website/rubriekenpagina.php
+++ Website/rubriekenpagina.php
@@ -10,40 +10,43 @@
 include_once 'aanroepingen/RubNavMobiel.php'
 ?>
 </aside>
-
+<div class="row columns"> 
+  <nav aria-label="You are here:" role="navigation">
+    <ul class="breadcrumbs">
+      <li><a href="Index.php">Home</a></li>
+      <li><a href="#">Antiek en Kunst</a></li>
+      <li>    
+      <span class="show-for-sr">Current: </span> Kasten
+      </li>
+    </ul>
+  </nav>
+</div>
 <br>
 <div class="rubrieken">
-    <div class = "marginlinks">
+<div class ="tabelvorm">
     <input type="radio" name="Kaas" value="op">Prijs Oplopend<br>
     <input type="radio" name="Kaas" value="af">Prijs Aflopend<br>
     <input type="radio" name="Kaas" value="op">Looptijd Oplopend<br>
     <input type="radio" name="Kaas" value="af">Looptijd Aflopend<br>
-    </div>
-    <div>
-    <ul>
+</div>
+
+<div class="tabelvorm">
         <h3>Locatie</h3>
-            <select class = "meerkeuzevak"> 
+        <select class = "meerkeuzevak"> 
           <option>Kies een plaats</option>
           <option>Den Haag</option>
           <option>Amsterdam</option>
           <option>Rotterdam</option>
           <option>Arnhem</option>
           </select>
-            </ul>
-    </div>
-    <div>
-    <ul>
+</div>
+<div class="tabelvorm">
         <h3>Prijs</h3>
-
  <p> Van: </p> <input type="text"> 
- <p> Tot: </p> <input type="text"> 
+ <p> Tot: </p> <input type="text">
 </div>
 </div>
-</div>
-</div>
-            </ul>
-    </div>
-</div>
+
     <div class="rubriekenContainer">
     <article class="RubriekenProducten" >
         <h3>Een Mooie Kaaskast</h3>
@@ -55,8 +58,8 @@ include_once 'aanroepingen/RubNavMobiel.php'
         <p>Locatie: Arnhem</p>
         <p>Huidige hoogste bod:</p>
         <h4>189,99</h4>
-        <p> Veiling sluit over: </p>
-        <h4> 6d 11u 12m </h4>
+        <p>Veiling sluit over: </p>
+        <h4>6d 11u 12m</h4>
         </div>
     </div>
     </article>
@@ -64,7 +67,7 @@ include_once 'aanroepingen/RubNavMobiel.php'
     <div class="rubriekenContainer">
     <article class="RubriekenProducten">
     <h3>Een Geweldige Rubberen Eend</h3>
-    <div class ="rechts">
+    <div class = "rechts"> 
        <input type='submit'value='Bieden!' onclick="window.location.href = 'product.php';" class='button'>
        </div>
         <div class ="center">
@@ -72,15 +75,14 @@ include_once 'aanroepingen/RubNavMobiel.php'
         <p>Locatie: Arnhem</p>
         <p>Huidige hoogste bod:</p>
         <h4>189,99</h4>
-        <p> Veiling sluit over: </p>
-        <h4> 6d 11u 12m </h4>
-       </div>
+        <p>Veiling sluit over:</p>
+        <h4>6d 11u 12m</h4>
        </div>
     </article>
 
     <div class="rubriekenContainer">
     <article class="RubriekenProducten">
-    <h3>Een     Salade wel over datum</h3>
+    <h3>Een Salade wel over datum</h3>
     <div class ="rechts">
        <input type='submit'value='Bieden!' onclick="window.location.href = 'product.php';" class='button'>
        </div>
@@ -89,12 +91,12 @@ include_once 'aanroepingen/RubNavMobiel.php'
         <p>Locatie: Arnhem</p>
         <p>Huidige hoogste bod:</p>
         <h4>189,99</h4>
-        <p> Veiling sluit over: </p>
-        <h4> 6d 11u 12m </h4>
-       </div>
-    </article>
-
-    </div>
+        <p>Veiling sluit over:</p>
+        <h4>6d 11u 12m</h4>
+        </div>
+        </article>
+        </div>
+        </div>
 <?php
 include_once 'aanroepingen/footer.html';
 ?>
