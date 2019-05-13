937914dc06f7cc6be06eefd96ce142d107c264c5
memo043 <mehmet.batal@hotmail.com>
2019-05-13 11:08:56 +0200

    Update Index.php

--- Website/Index.php
+++ Website/Index.php
@@ -1,14 +1,4 @@
-<!doctype html>
-<html class="no-js" lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Foundation for Sites</title>
-    <link rel="stylesheet" href="css/foundation.css">
-    <link rel="stylesheet" href="css/styles.css">
-  </head>
-  <body>
+
     <div class="holy-grail-grid">
   <div class="holy-grail-header">
     <?php  include_once 'aanroepingen/header.php'?>
@@ -33,7 +23,3 @@
 </div>
 
 
-
-    
-  </body>
-</html>
