6793c589bba5af2dd764d7c2fb04765c6b956e1d
Danny <48557943+P00PF4C3@users.noreply.github.com>
2019-05-14 10:14:07 +0200

    Product HTML

--- Website/overons.php
+++ Website/overons.php
@@ -1,9 +1,11 @@
     <?php
+      $config = ['pagina' => 'overons'];
+
       require_once 'aanroepingen/connectie.php';
       include_once 'aanroepingen/header.php';
     ?>
 
-    <h1> Over EenmaalAndermaal </h1>
+    <h1>Over EenmaalAndermaal</h1>
     <p> 
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae malesuada felis, ac scelerisque risus. Suspendisse mollis lorem leo, nec tempus urna porttitor vitae. Pellentesque nunc est, mollis nec nisi vel, molestie bibendum mauris. Nullam eu porta nisl, sed euismod est. Cras tellus nisi, posuere sit amet ligula in, dictum scelerisque ligula. Ut nec risus iaculis, ullamcorper felis eget, ultrices odio. Integer vel purus id neque sodales imperdiet. Mauris congue turpis elit, ac pharetra lacus volutpat a. Sed non gravida nulla. Etiam sem diam, imperdiet sit amet sem eget, lobortis egestas nunc. Ut tellus mi, lacinia non sem pellentesque, suscipit pretium magna.
       Ut vitae turpis tincidunt mi blandit ullamcorper. Vivamus vitae dolor porttitor, tempus turpis non, pulvinar quam. Duis placerat sem sem, sit amet bibendum odio luctus eget. Integer quis orci lorem. Curabitur vitae auctor nisi. Nullam at dui tristique, consequat felis a, ullamcorper nisi. Vestibulum purus erat, luctus non dolor in, cursus dictum ex. Nulla urna arcu, efficitur eget odio at, ornare suscipit odio. In non pretium sapien. Duis efficitur, lorem quis maximus pulvinar, orci erat eleifend neque, non faucibus enim nisl et lectus. Proin sagittis nulla at risus mollis, et feugiat turpis facilisis.
