df315862481f74dd351bc153392335714d707af1
memo043 <mehmet.batal@hotmail.com>
2019-05-14 09:37:33 +0200

    homegpage titels,css producten
    <Hr> onder de titels weggehaald, producten op homepage hebben border-bottom

--- Website/Index.php
+++ Website/Index.php
@@ -9,91 +9,91 @@
   </div>
 
   <div class="holy-grail-middle">
-  <h3> De populairste veilingen</h3>
-  <hr>
+  <h3 class="HomePageTitel"> De populairste veilingen</h3>
+  
     <div class="ProductenContainer">
       <div class="Product">
         <img src="/images/fiets.jpg" >
         <h3>Viking fiets</h3>
-        <p>tijd:99:99:99 <span style="float:right"> $1999999999.99</span></p>
+        <p>tijd:99:99:99 <span style="float:right">Hoogste bod: $19.99</span></p>
         <button>Bekijk meer</button>
       </div>  
       <div class="Product">
         <img src="/images/fiets.jpg" >
         <h3>Viking fiets</h3>
-        <p>tijd:99:99:99 <span style="float:right"> $19.99</span></p>
+        <p>tijd:99:99:99 <span style="float:right">Hoogste bod: $19.99</span></p>
         <button>Bekijk meer</button>
       </div>  
       <div class="Product">
         <img src="/images/fiets.jpg" >
         <h3>Viking fiets</h3>
-        <p>tijd:99:99:99 <span style="float:right"> $19.99</span></p>
+        <p>tijd:99:99:99 <span style="float:right">Hoogste bod: $19.99</span></p>
         <button>Bekijk meer</button>
       </div> 
       <div class="Product">
         <img src="/images/fiets.jpg" >
         <h3>Viking fiets</h3>
-        <p>tijd:99:99:99 <span style="float:right"> $19.99</span></p>
+        <p>tijd:99:99:99 <span style="float:right">Hoogste bod: $19.99</span></p>
         <button>Bekijk meer</button>
       </div> 
      
     </div>
 
 
-    <h3>Loopt bijna af! </h3>
-    <hr>
+    <h3 class="HomePageTitel">Loopt bijna af! </h3>
+    
     <div class="ProductenContainer">
       <div class="Product">
         <img src="/images/fiets.jpg" >
         <h3>Viking fiets</h3>
-        <p>tijd:99:99:99 <span style="float:right"> $1999999999.99</span></p>
+        <p>tijd:99:99:99 <span style="float:right">Hoogste bod: $19.99</span></p>
         <button>Bekijk meer</button>
       </div>  
       <div class="Product">
         <img src="/images/fiets.jpg" >
         <h3>Viking fiets</h3>
-        <p>tijd:99:99:99 <span style="float:right"> $19.99</span></p>
+        <p>tijd:99:99:99 <span style="float:right">Hoogste bod: $19.99</span></p>
         <button>Bekijk meer</button>
       </div>  
       <div class="Product">
         <img src="/images/fiets.jpg" >
         <h3>Viking fiets</h3>
-        <p>tijd:99:99:99 <span style="float:right"> $19.99</span></p>
+        <p>tijd:99:99:99 <span style="float:right">Hoogste bod: $19.99</span></p>
         <button>Bekijk meer</button>
       </div> 
       <div class="Product">
         <img src="/images/fiets.jpg" >
         <h3>Viking fiets</h3>
-        <p>tijd:99:99:99 <span style="float:right"> $19.99</span></p>
+        <p>tijd:99:99:99 <span style="float:right">Hoogste bod: $19.99</span></p>
         <button>Bekijk meer</button>
       </div> 
      
     </div>
-    <h3> Nieuwe veilingen</h3>
-    <hr>
+    <h3 class="HomePageTitel"> Nieuwe veilingen</h3>
+    
     <div class="ProductenContainer">
       <div class="Product">
         <img src="/images/fiets.jpg" >
         <h3>Viking fiets</h3>
-        <p>tijd:99:99:99 <span style="float:right"> $1999999999.99</span></p>
+        <p>tijd:99:99:99 <span style="float:right">Hoogste bod: $19.99</span></p>
         <button>Bekijk meer</button>
       </div>  
       <div class="Product">
         <img src="/images/fiets.jpg" >
         <h3>Viking fiets</h3>
-        <p>tijd:99:99:99 <span style="float:right"> $19.99</span></p>
+        <p>tijd:99:99:99 <span style="float:right">Hoogste bod: $19.99</span></p>
         <button>Bekijk meer</button>
       </div>  
       <div class="Product">
         <img src="/images/fiets.jpg" >
         <h3>Viking fiets</h3>
-        <p>tijd:99:99:99 <span style="float:right"> $19.99</span></p>
+        <p>tijd:99:99:99 <span style="float:right">Hoogste bod: $19.99</span></p>
         <button>Bekijk meer</button>
       </div> 
       <div class="Product">
         <img src="/images/fiets.jpg" >
         <h3>Viking fiets</h3>
-        <p>tijd:99:99:99 <span style="float:right"> $19.99</span></p>
+        <p>tijd:99:99:99 <span style="float:right">Hoogste bod: $19.99</span></p>
         <button>Bekijk meer</button>
       </div> 
       
