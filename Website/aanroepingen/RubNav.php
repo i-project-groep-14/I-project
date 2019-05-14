e652a06746345808f6565d862f7d832cfcb46f2d
HendrikOtten28 <44369979+HendrikOtten28@users.noreply.github.com>
2019-05-14 14:55:06 +0200

    f

--- Website/aanroepingen/RubNav.php
+++ Website/aanroepingen/RubNav.php
@@ -35,213 +35,213 @@
           <li>
             <a href="rubriekenpagina.php">Item 1B</a>
             <ul class="menu">
-              <li><a href="rubriekenpagina.phpBi">Item 1B i</a></li>
-              <li><a href="rubriekenpagina.phpBii">Item 1B ii</a></li>
+              <li><a href="rubriekenpagina.php">Item 1B i</a></li>
+              <li><a href="rubriekenpagina.php">Item 1B ii</a></li>
               <li>
-                <a href="rubriekenpagina.phpBiii">Item 1B iii</a>
+                <a href="rubriekenpagina.php">Item 1B iii</a>
                 <ul class="menu">
-                  <li><a href="rubriekenpagina.phpBiiialpha">Item 1B iii alpha</a></li>
-                  <li><a href="rubriekenpagina.phpBiiiomega">Item 1B iii omega</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iii alpha</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iii omega</a></li>
                 </ul>
               </li>
               <li>
-                <a href="rubriekenpagina.phpBiv">Item 1B iv</a>
+                <a href="rubriekenpagina.php">Item 1B iv</a>
                 <ul class="menu">
-                  <li><a href="rubriekenpagina.phpBivalpha">Item 1B iv alpha</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iv alpha</a></li>
                 </ul>
               </li>
             </ul>
           </li>
-          <li><a href="rubriekenpagina.phpC">Item 1C</a></li>
+          <li><a href="rubriekenpagina.php">Item 1C</a></li>
         </ul>
       </li>
 	  <li>
         <a href="rubriekenpagina.php">Auto's en Fietsen</a>
         <ul class="menu">
-          <li><a href="rubriekenpagina.phpA">Auto's en Fietsen</a></li>
+          <li><a href="rubriekenpagina.php">Auto's en Fietsen</a></li>
           <li>
-            <a href="rubriekenpagina.phpB">Item 1B</a>
+            <a href="rubriekenpagina.php">Item 1B</a>
             <ul class="menu">
-              <li><a href="rubriekenpagina.phpBi">Item 1B i</a></li>
-              <li><a href="rubriekenpagina.phpBii">Item 1B ii</a></li>
+              <li><a href="rubriekenpagina.php">Item 1B i</a></li>
+              <li><a href="rubriekenpagina.php">Item 1B ii</a></li>
               <li>
-                <a href="rubriekenpagina.phpBiii">Item 1B iii</a>
+                <a href="rubriekenpagina.php">Item 1B iii</a>
                 <ul class="menu">
-                  <li><a href="rubriekenpagina.phpBiiialpha">Item 1B iii alpha</a></li>
-                  <li><a href="rubriekenpagina.phpBiiiomega">Item 1B iii omega</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iii alpha</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iii omega</a></li>
                 </ul>
               </li>
               <li>
-                <a href="rubriekenpagina.phpBiv">Item 1B iv</a>
+                <a href="rubriekenpagina.php">Item 1B iv</a>
                 <ul class="menu">
-                  <li><a href="rubriekenpagina.phpBivalpha">Item 1B iv alpha</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iv alpha</a></li>
                 </ul>
               </li>
             </ul>
           </li>
-          <li><a href="rubriekenpagina.phpC">Item 1C</a></li>
+          <li><a href="rubriekenpagina.php">Item 1C</a></li>
         </ul>
       </li>
 	  <li>
         <a href="rubriekenpagina.php">Auto's en Fietsen</a>
         <ul class="menu">
-          <li><a href="rubriekenpagina.phpA">Auto's en Fietsen</a></li>
+          <li><a href="rubriekenpagina.php">Auto's en Fietsen</a></li>
           <li>
-            <a href="rubriekenpagina.phpB">Item 1B</a>
+            <a href="rubriekenpagina.php">Item 1B</a>
             <ul class="menu">
-              <li><a href="rubriekenpagina.phpBi">Item 1B i</a></li>
-              <li><a href="rubriekenpagina.phpBii">Item 1B ii</a></li>
+              <li><a href="rubriekenpagina.php">Item 1B i</a></li>
+              <li><a href="rubriekenpagina.php">Item 1B ii</a></li>
               <li>
-                <a href="rubriekenpagina.phpBiii">Item 1B iii</a>
+                <a href="rubriekenpagina.php">Item 1B iii</a>
                 <ul class="menu">
-                  <li><a href="rubriekenpagina.phpBiiialpha">Item 1B iii alpha</a></li>
-                  <li><a href="rubriekenpagina.phpBiiiomega">Item 1B iii omega</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iii alpha</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iii omega</a></li>
                 </ul>
               </li>
               <li>
-                <a href="rubriekenpagina.phpBiv">Item 1B iv</a>
+                <a href="rubriekenpagina.php">Item 1B iv</a>
                 <ul class="menu">
-                  <li><a href="rubriekenpagina.phpBivalpha">Item 1B iv alpha</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iv alpha</a></li>
                 </ul>
               </li>
             </ul>
           </li>
-          <li><a href="rubriekenpagina.phpC">Item 1C</a></li>
+          <li><a href="rubriekenpagina.php">Item 1C</a></li>
         </ul>
 	  </li>
 	  <li>
         <a href="rubriekenpagina.php">Auto's en Fietsen</a>
         <ul class="menu">
-          <li><a href="rubriekenpagina.phpA">Auto's en Fietsen</a></li>
+          <li><a href="rubriekenpagina.php">Auto's en Fietsen</a></li>
           <li>
-            <a href="rubriekenpagina.phpB">Item 1B</a>
+            <a href="rubriekenpagina.php">Item 1B</a>
             <ul class="menu">
-              <li><a href="rubriekenpagina.phpBi">Item 1B i</a></li>
-              <li><a href="rubriekenpagina.phpBii">Item 1B ii</a></li>
+              <li><a href="rubriekenpagina.php">Item 1B i</a></li>
+              <li><a href="rubriekenpagina.php">Item 1B ii</a></li>
               <li>
-                <a href="rubriekenpagina.phpBiii">Item 1B iii</a>
+                <a href="rubriekenpagina.php">Item 1B iii</a>
                 <ul class="menu">
-                  <li><a href="rubriekenpagina.phpBiiialpha">Item 1B iii alpha</a></li>
-                  <li><a href="rubriekenpagina.phpBiiiomega">Item 1B iii omega</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iii alpha</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iii omega</a></li>
                 </ul>
               </li>
               <li>
-                <a href="rubriekenpagina.phpBiv">Item 1B iv</a>
+                <a href="rubriekenpagina.php">Item 1B iv</a>
                 <ul class="menu">
-                  <li><a href="rubriekenpagina.phpBivalpha">Item 1B iv alpha</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iv alpha</a></li>
                 </ul>
               </li>
             </ul>
           </li>
-          <li><a href="rubriekenpagina.phpC">Item 1C</a></li>
+          <li><a href="rubriekenpagina.php">Item 1C</a></li>
         </ul>
 	  </li>
 	  <li>
         <a href="rubriekenpagina.php">Auto's en Fietsen</a>
         <ul class="menu">
-          <li><a href="rubriekenpagina.phpA">Auto's en Fietsen</a></li>
+          <li><a href="rubriekenpagina.php">Auto's en Fietsen</a></li>
           <li>
-            <a href="rubriekenpagina.phpB">Item 1B</a>
+            <a href="rubriekenpagina.php">Item 1B</a>
             <ul class="menu">
-              <li><a href="rubriekenpagina.phpBi">Item 1B i</a></li>
-              <li><a href="rubriekenpagina.phpBii">Item 1B ii</a></li>
+              <li><a href="rubriekenpagina.php">Item 1B i</a></li>
+              <li><a href="rubriekenpagina.php">Item 1B ii</a></li>
               <li>
-                <a href="rubriekenpagina.phpBiii">Item 1B iii</a>
+                <a href="rubriekenpagina.php">Item 1B iii</a>
                 <ul class="menu">
-                  <li><a href="rubriekenpagina.phpBiiialpha">Item 1B iii alpha</a></li>
-                  <li><a href="rubriekenpagina.phpBiiiomega">Item 1B iii omega</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iii alpha</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iii omega</a></li>
                 </ul>
               </li>
               <li>
-                <a href="rubriekenpagina.phpBiv">Item 1B iv</a>
+                <a href="rubriekenpagina.php">Item 1B iv</a>
                 <ul class="menu">
-                  <li><a href="rubriekenpagina.phpBivalpha">Item 1B iv alpha</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iv alpha</a></li>
                 </ul>
               </li>
             </ul>
           </li>
-          <li><a href="rubriekenpagina.phpC">Item 1C</a></li>
+          <li><a href="rubriekenpagina.php">Item 1C</a></li>
         </ul>
 	  </li>
 	  <li>
         <a href="rubriekenpagina.php">Auto's en Fietsen</a>
         <ul class="menu">
-          <li><a href="rubriekenpagina.phpA">Auto's en Fietsen</a></li>
+          <li><a href="rubriekenpagina.php">Auto's en Fietsen</a></li>
           <li>
-            <a href="rubriekenpagina.phpB">Item 1B</a>
+            <a href="rubriekenpagina.php">Item 1B</a>
             <ul class="menu">
-              <li><a href="rubriekenpagina.phpBi">Item 1B i</a></li>
-              <li><a href="rubriekenpagina.phpBii">Item 1B ii</a></li>
+              <li><a href="rubriekenpagina.php">Item 1B i</a></li>
+              <li><a href="rubriekenpagina.php">Item 1B ii</a></li>
               <li>
-                <a href="rubriekenpagina.phpBiii">Item 1B iii</a>
+                <a href="rubriekenpagina.php">Item 1B iii</a>
                 <ul class="menu">
-                  <li><a href="rubriekenpagina.phpBiiialpha">Item 1B iii alpha</a></li>
-                  <li><a href="rubriekenpagina.phpBiiiomega">Item 1B iii omega</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iii alpha</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iii omega</a></li>
                 </ul>
               </li>
               <li>
-                <a href="rubriekenpagina.phpBiv">Item 1B iv</a>
+                <a href="rubriekenpagina.php">Item 1B iv</a>
                 <ul class="menu">
-                  <li><a href="rubriekenpagina.phpBivalpha">Item 1B iv alpha</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iv alpha</a></li>
                 </ul>
               </li>
             </ul>
           </li>
-          <li><a href="rubriekenpagina.phpC">Item 1C</a></li>
+          <li><a href="rubriekenpagina.php">Item 1C</a></li>
         </ul>
 	  </li>
 	  <li>
         <a href="rubriekenpagina.php">Auto's en Fietsen</a>
         <ul class="menu">
-          <li><a href="rubriekenpagina.phpA">Auto's en Fietsen</a></li>
+          <li><a href="rubriekenpagina.php">Auto's en Fietsen</a></li>
           <li>
-            <a href="rubriekenpagina.phpB">Item 1B</a>
+            <a href="rubriekenpagina.php">Item 1B</a>
             <ul class="menu">
-              <li><a href="rubriekenpagina.phpBi">Item 1B i</a></li>
-              <li><a href="rubriekenpagina.phpBii">Item 1B ii</a></li>
+              <li><a href="rubriekenpagina.php">Item 1B i</a></li>
+              <li><a href="rubriekenpagina.php">Item 1B ii</a></li>
               <li>
-                <a href="rubriekenpagina.phpBiii">Item 1B iii</a>
+                <a href="rubriekenpagina.php">Item 1B iii</a>
                 <ul class="menu">
-                  <li><a href="rubriekenpagina.phpBiiialpha">Item 1B iii alpha</a></li>
-                  <li><a href="rubriekenpagina.phpBiiiomega">Item 1B iii omega</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iii alpha</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iii omega</a></li>
                 </ul>
               </li>
               <li>
-                <a href="rubriekenpagina.phpBiv">Item 1B iv</a>
+                <a href="rubriekenpagina.php">Item 1B iv</a>
                 <ul class="menu">
-                  <li><a href="rubriekenpagina.phpBivalpha">Item 1B iv alpha</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iv alpha</a></li>
                 </ul>
               </li>
             </ul>
           </li>
-          <li><a href="rubriekenpagina.phpC">Item 1C</a></li>
+          <li><a href="rubriekenpagina.php">Item 1C</a></li>
         </ul>
 	  </li>
 	  <li>
         <a href="rubriekenpagina.php">Auto's en Fietsen</a>
         <ul class="menu">
-          <li><a href="rubriekenpagina.phpA">Auto's en Fietsen</a></li>
+          <li><a href="rubriekenpagina.php">Auto's en Fietsen</a></li>
           <li>
-            <a href="rubriekenpagina.phpB">Item 1B</a>
+            <a href="rubriekenpagina.php">Item 1B</a>
             <ul class="menu">
-              <li><a href="rubriekenpagina.phpBi">Item 1B i</a></li>
-              <li><a href="rubriekenpagina.phpBii">Item 1B ii</a></li>
+              <li><a href="rubriekenpagina.php">Item 1B i</a></li>
+              <li><a href="rubriekenpagina.php">Item 1B ii</a></li>
               <li>
-                <a href="rubriekenpagina.phpBiii">Item 1B iii</a>
+                <a href="rubriekenpagina.php">Item 1B iii</a>
                 <ul class="menu">
-                  <li><a href="rubriekenpagina.phpBiiialpha">Item 1B iii alpha</a></li>
-                  <li><a href="rubriekenpagina.phpBiiiomega">Item 1B iii omega</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iii alpha</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iii omega</a></li>
                 </ul>
               </li>
               <li>
-                <a href="rubriekenpagina.phpBiv">Item 1B iv</a>
+                <a href="rubriekenpagina.php">Item 1B iv</a>
                 <ul class="menu">
-                  <li><a href="rubriekenpagina.phpBivalpha">Item 1B iv alpha</a></li>
+                  <li><a href="rubriekenpagina.php">Item 1B iv alpha</a></li>
                 </ul>
               </li>
             </ul>
           </li>
-          <li><a href="rubriekenpagina.phpC">Item 1C</a></li>
+          <li><a href="rubriekenpagina.php">Item 1C</a></li>
         </ul>
       </li>
     </ul>
