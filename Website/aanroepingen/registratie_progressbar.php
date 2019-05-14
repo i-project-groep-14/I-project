b324be3b66164878877bf50e55647b362aa0d8cb
Danny <48557943+P00PF4C3@users.noreply.github.com>
2019-05-14 10:22:59 +0200

    Registratie_email HTML

--- Website/aanroepingen/registratie_progressbar.php
+++ Website/aanroepingen/registratie_progressbar.php
@@ -1,59 +1,56 @@
 <?php
-echo"
-<div class='body-tekst' >
-<ol class='progress-indicator'>
-  <li class=";
-  if($config['pagina'] == 'registratie_email') {
-        echo"'is-current'";
-  } else {
-        echo "is-complete";
-  }
-  echo "
-  data-step=''>
-    <span>Verifiëren e-mail</span>
-  </li>
-  <li class=";
-  if($config['pagina'] == 'registratie_persoonsgegevens') {
-        echo"'is-current'";
-  } else if ($config['pagina'] == 'registratie_email') {
-        echo "''";
-      }else {
-        echo "is-complete";
-  }
-  echo "
-  data-step=''>
-    <span>Gegevens invullen</span>
-  </li>
-  <li class=";
-  if($config['pagina'] == 'registratie_vraag') {
-        echo"'is-current'";
-  }else if($config['pagina'] == 'registratie_email'){
-      echo"''";
-  } else if($config['pagina'] == 'registratie_persoonsgegevens'){
-      echo"''";
-  } else {
-        echo "is-complete";
-  }
-  echo"
-  data-step=''>
-    <span>Veiligheidsvraag</span>
-  </li>
-  
-</div>
-</ol>";
+      echo"
+      <div class='body-tekst' >
+            <ol class='progress-indicator'>
+                  <li class=";
+                        if ($config['pagina'] == 'registratie_email') {
+                              echo"'is-current'";
+                        } else {
+                              echo "is-complete";
+                        }
+                        echo "
+                        data-step=''>
+                        <span>Verifiëren e-mail</span>
+                  </li>
+                  <li class=";
+                        if($config['pagina'] == 'registratie_persoonsgegevens') {
+                              echo"'is-current'";
+                        } else if ($config['pagina'] == 'registratie_email') {
+                              echo "''";
+                        } else {
+                              echo "is-complete";
+                        }
+                        echo "
+                        data-step=''>
+                        <span>Gegevens invullen</span>
+                  </li>
+                  <li class=";
+                        if($config['pagina'] == 'registratie_vraag') {
+                              echo"'is-current'";
+                        } else if ($config['pagina'] == 'registratie_email' || $config['pagina'] == 'registratie_persoonsgegevens'){
+                              echo"''";
+                        } else {
+                              echo "is-complete";
+                        }
+                        echo"
+                        data-step=''>
+                        <span>Veiligheidsvraag</span>
+                  </li>
+            </ol>
+      </div>";
 
-// <ol class="progress-indicator">
-//   <li class="is-complete" data-step="">
-//     <span>Verifiëren e-mail</span>
-//   </li>
-//   <li class="is-complete" data-step="">
-//     <span>Gegevens invullen</span>
-//   </li>
-//   <li class="is-current" data-step="">
-//     <span>Veiligheid</span>
-//   </li>
-//   <li class="" data-step="">
-//       <span>Klaar</span>
-// </li>
-// </ol>
+      // <ol class="progress-indicator">
+      //   <li class="is-complete" data-step="">
+      //     <span>Verifiëren e-mail</span>
+      //   </li>
+      //   <li class="is-complete" data-step="">
+      //     <span>Gegevens invullen</span>
+      //   </li>
+      //   <li class="is-current" data-step="">
+      //     <span>Veiligheid</span>
+      //   </li>
+      //   <li class="" data-step="">
+      //       <span>Klaar</span>
+      // </li>
+      // </ol>
 ?>
