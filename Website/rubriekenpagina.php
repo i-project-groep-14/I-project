bcbbc6743f6d28afa1632480c7e05eb912af6e76
HendrikOtten28 <44369979+HendrikOtten28@users.noreply.github.com>
2019-05-14 10:21:00 +0200

    Rubriekenpagina gefixt alleen bovenste filter is lelijk en niet responsive rest wel

--- Website/rubriekenpagina.php
+++ Website/rubriekenpagina.php
@@ -1,10 +1,18 @@
 <?php
   $config = ['pagina' => 'rubriekenpagina'];
-
   require_once 'aanroepingen/connectie.php';
-  include_once 'aanroepingen/header.php';
 ?>
 
+<div class="holy-grail-grid">
+  <div class="holy-grail-header">
+    <?php  include_once 'aanroepingen/header.php'?>
+  </div>
+
+  <div class="holy-grail-left">
+  <?php   include_once 'aanroepingen/RubNav.php'?>
+  </div>
+
+  <div class="holy-grail-middle">
 <div class="row columns"> 
   <nav aria-label="You are here:" role="navigation">
     <ul class="breadcrumbs">
@@ -17,15 +25,15 @@
   </nav>
 </div>
 <br>
-
-<div class="rubrieken">
-<div class ="tabelvorm">
+<div class="rubriekenTop">
+<div class ="tabelvorm-links">
+  <h3> Filters </h3>
     <input type="radio" name="Kaas" value="op">Prijs Oplopend<br>
     <input type="radio" name="Kaas" value="af">Prijs Aflopend<br>
     <input type="radio" name="Kaas" value="op">Looptijd Oplopend<br>
     <input type="radio" name="Kaas" value="af">Looptijd Aflopend<br>
 </div>
-<div class="tabelvorm">
+<div class="tabelvorm-midden">
         <h3>Locatie</h3>
         <select class = "meerkeuzevak"> 
           <option>Kies een plaats</option>
@@ -35,13 +43,16 @@
           <option>Arnhem</option>
           </select>
 </div>
-<div class="tabelvorm">
+<div class="tabelvorm-rechts">
         <h3>Prijs</h3>
+        <div class="prijsbreedte">
  <p> Van: </p> <input type="text"> 
  <p> Tot: </p> <input type="text">
 </div>
 </div>
+</div>
 
+<br>
     <div class="rubriekenContainer">
     <article class="RubriekenProducten" >
         <h3>Een Mooie Kaaskast</h3>
@@ -50,7 +61,7 @@
         <p>Locatie: Arnhem</p>
         <p>Huidige hoogste bod:</p>
         <h4>189,99</h4>
-        <p>Veiling sluit over: </p>
+        <p>Veiling sluit over:</p>
         <h4>6d 11u 12m</h4>
         <div class ="rechts">
        <input type='submit'value='Bieden!' onclick="window.location.href = 'product.php';" class='button'>
@@ -79,7 +90,7 @@
     <article class="RubriekenProducten">
     <h3>Een Salade wel over datum</h3>
         <div class ="center">
-       <img src="images/Salade.jpg"  alt="filmje1"/>
+        <img src="images/Salade.jpg" alt="filmje1"/>
         <p>Locatie: Arnhem</p>
         <p>Huidige hoogste bod:</p>
         <h4>189,99</h4>
@@ -92,9 +103,11 @@
         </article>
         </div>
         </div>
-<?php
-include_once 'aanroepingen/footer.html';
-?>
+        </div>
+
+        <div class="holy-grail-footer">
+    <?php include_once 'aanroepingen/footer.html' ?>
+  </div>
 
 
 
