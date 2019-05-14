d8ad934f228954ff0026cbaf11123f8e3ae38b66
Danny <48557943+P00PF4C3@users.noreply.github.com>
2019-05-14 15:12:42 +0200

    Database aangepast

--- Database/Createscript.sql
+++ Database/Createscript.sql
@@ -21,21 +21,21 @@ use EenmaalAndermaal
 /* Table: Gebruiker												*/
 /*==============================================================*/
 create table gebruiker (
-   gebruikersnaam			varchar(20)			not null,
-   voornaam					varchar(20)			not null,
-   achternaam				varchar(20)			not null,
-   adresregel1				varchar(20)			not null,
-   adresregel2				varchar(20)			null,
-   postcode					char(7)				not null,
-   plaatsnaam				varchar(20)			not null,
-   land						varchar(20)			not null,
-   geboortedatum			date				not null,
-   mailadres				varchar(50)			not null,
-   wachtwoord				varchar(255)		not null,
-   vraag					numeric(2)			not null,
-   antwoordtekst			varchar(50)			not null,
-   rol						numeric(1)			not null,
-   profielfoto				varchar(255)		null
+   gebruikersnaam				varchar(20)			not null,
+   voornaam						varchar(20)			not null,
+   achternaam					varchar(20)			not null,
+   adresregel1					varchar(20)			not null,
+   adresregel2					varchar(20)			null,
+   postcode						char(7)				not null,
+   plaatsnaam					varchar(20)			not null,
+   land							varchar(20)			not null,
+   geboortedatum				date				not null,
+   mailadres					varchar(50)			not null,
+   wachtwoord					varchar(255)		not null,
+   vraag						numeric(2)			not null,
+   antwoordtekst				varchar(50)			not null,
+   rol							numeric(1)			not null,
+   profielfoto					varchar(255)		null
    constraint pk_gebruiker_gebruikersnaam primary key (gebruikersnaam)
 )	
 
@@ -43,8 +43,8 @@ create table gebruiker (
 /* Table: Vraag													*/
 /*==============================================================*/
 create table vraag (
-	vraagnummer				numeric(2)			not null,
-	tekstvraag				varchar(50)			not null
+	vraagnummer					numeric(2)			not null,
+	tekstvraag					varchar(50)			not null
 	constraint pk_vraag_vraagnummer primary key (vraagnummer)
 )
 
@@ -52,25 +52,121 @@ create table vraag (
 /* Table: Gebruikerstelefoon									*/
 /*==============================================================*/
 create table gebruikerstelefoon (
-	volgnr					numeric(2)			identity(1,1) not null,
-	gebruiker				varchar(20)			not null,
-	telefoon				char(11)			not null
+	volgnr						numeric(3)			identity(1,1) not null,
+	gebruiker					varchar(20)			not null,
+	telefoon					char(11)			not null
 	constraint pk_gebruikerstelefoon_volgnr primary key (volgnr),
-	constraint fk_gebruikerstelefoon_gebruiker foreign key (gebruiker) references gebruiker (gebruikersnaam)
+	constraint fk_gebruikerstelefoon_gebruiker foreign key (gebruiker) references gebruiker (gebruikersnaam) on update cascade on delete no action
 )
 
 alter table gebruiker
-add constraint fk_gebruiker_vraag foreign key (vraag) references vraag (vraagnummer),
+add constraint fk_gebruiker_vraag foreign key (vraag) references vraag (vraagnummer) on update no action on delete no action,
 	constraint ck_gebruiker_geboortedatumVoorVandaag check (geboortedatum < getDate()),
 	constraint ck_gebruiker_wachtwoordLangerDan7Karakters check (LEN(wachtwoord) >= 7),
 	constraint ck_gebruiker_wachtwoordMinimaal1LetterEn1Cijfer check (wachtwoord like '%[A-Za-z]%' and wachtwoord like '%[0-9]%')
-	
+
+
+
+/*==============================================================*/
+/* Table: Verkoper												*/
+/*==============================================================*/
+create table verkoper (
+	gebruiker					varchar(20)			not null,
+	bank						varchar(20)			null,
+	bankrekening				varchar(30)			null,
+	[controle-optie]			varchar(20)			not null,
+	creditcard					varchar(30)			null
+	constraint pk_verkoper_gebruiker primary key (gebruiker),
+	constraint fk_verkoper_gebruiker foreign key (gebruiker) references gebruiker (gebruikersnaam) on update cascade on delete no action
+)
+
+/*==============================================================*/
+/* Table: Voorwerp												*/
+/*==============================================================*/
+create table voorwerp (
+	voorwerpnummer				int					identity(1,1) not null,
+	titel						varchar(30)			not null,
+	beschrijving				varchar(100)		not null,
+	startprijs					varchar(10)			not null,
+	betalingswijze				varchar(20)			not null,
+	betalingsinstructie			varchar(30)			null,
+	plaatsnaam					varchar(20)			not null,
+	land						varchar(20)			not null,
+	looptijd					numeric(3)			not null,
+	looptijdbeginDag			date				not null,
+	looptijdbeginTijdstip		char(8)				not null,
+	verzendkosten				varchar(10)			null,
+	verzendinstructies			varchar(100)		null,
+	verkoper					varchar(20)			not null,
+	koper						varchar(20)			null,
+	looptijdeindeDag			date				not null,
+	looptijdeindeTijdstip		char(8)				not null,
+	[veilingGesloten?]			varchar(4)			not null,
+	verkoopprijs				varchar(10)			null
+	constraint pk_voorwerp_voorwerpnummer primary key (voorwerpnummer),
+	constraint fk_voorwerp_verkoper foreign key (verkoper) references verkoper (gebruiker),
+	constraint fk_voorwerp_koper foreign key (koper) references gebruiker (gebruikersnaam) on update cascade on delete no action
+	/* bij rubriek een foreign key naar voorwerpnummer */
+
+	/*
+	AF 1	Tabel Voorwerp, kolom LooptijdeindeDag:
+	Kolom LooptijdeindeDag heeft de datum van LooptijdbeginDag + het aantal dagen dat Looptijd aangeeft.
+
+	AF 2	Tabel Voorwerp, kolom LooptijdeindeTijdstip:
+	Kolom LooptijdeindeTijdstip heeft dezelfde waarde als kolom LooptijdbeginTijdstip.
+
+	AF 3	Tabel Voorwerp, kolom VeilingGesloten?:
+	Kolom VeilingGesloten? heeft de waarde ‘niet’ als de systeemdatum en –tijd vroeger zijn dan wat kolommen LooptijdeindeDag en 
+	LooptijdeindeTijdstip aangeven, en de waarde ‘wel’ als de systeemdatum en –tijd later zijn dan dat.
+
+	AF 4	Tabel Voorwerp, kolom Koper:
+	Kolom Koper heeft een NULL-waarde, tenzij de veiling is gesloten en er op het voorwerp een bod is uitgebracht. Dan heeft kolom Koper 
+	de waarde uit kolom Bod(Gebruiker) die bij het hoogste bod op hetzelfde voorwerp hoort.
+
+	AF 5	Tabel Voorwerp, kolom Verkoopprijs:
+	Kolom Verkoopprijs heeft een NULL-waarde, tenzij de veiling is gesloten en er op het voorwerp een bod is uitgebracht. Dan heeft kolom 
+	Verkoopprijs de waarde uit kolom Bod(Bodbedrag) die bij het hoogste bod op hetzelfde voorwerp hoort.
+	*/
+)
+
+/*==============================================================*/
+/* Table: Bestand												*/
+/*==============================================================*/
+create table bestand (
+	filenaam					varchar(200)		not null,
+	voorwerp					int					not null
+	constraint pk_bestand_filenaam primary key (filenaam),
+	constraint fk_bestand_voorwerp foreign key (voorwerp) references voorwerp (voorwerpnummer) on update cascade on delete no action
+)
+
+/*==============================================================*/
+/* Table: Rubriek												*/
+/*==============================================================*/
+create table rubriek (
+	rubrieknummer				numeric(3)			not null,
+	rubrieknaam					varchar(50)			not null,
+	rubriek						numeric(3)			null,
+	volgnr						numeric(3)			not null
+	constraint pk_rubriek_rubrieknummer primary key (rubrieknummer),
+	constraint fk_rubriek_rubriek foreign key (rubriek) references rubriek (rubrieknummer) on update no action on delete no action
+)
+
+/*==============================================================*/
+/* Table: Voorwerp in rubriek									*/
+/*==============================================================*/
+create table [voorwerp in rubriek] (
+	voorwerp					int					not null,
+	[rubriek op laagste niveau]	numeric(3)			not null
+	constraint pk_voorwerpinrubriek_voorwerp primary key (voorwerp),
+	constraint fk_voorwerpinrubriek_voorwerp foreign key (voorwerp) references voorwerp (voorwerpnummer) on update cascade on delete no action,
+	constraint fk_voorwerpinrubriek_rubriekoplaagsteniveau foreign key ([rubriek op laagste niveau]) references rubriek (rubrieknummer) on update no action on delete no action
+)
 
 
 
 
 /*			Weghalen		*/
-insert into vraag values ('1', 'vraag1')
+insert into vraag values ('1', 'vraag1')	
 
 
 
