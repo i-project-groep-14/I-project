b81d94abb2ac089a8ac450478c15033fefc2343c
HendrikOtten28 <44369979+HendrikOtten28@users.noreply.github.com>
2019-05-15 13:46:11 +0200

    Homepagina

--- Website/beheerderspagina.php
+++ Website/beheerderspagina.php
@@ -7,7 +7,13 @@
     
     <div class="holy-grail-middle">
       <h3> Beheerders Pagina </h3>
-      <h4> Beheren van de rubrieken </h4>
+      <h4> Rubrieken Toevoegen </h4>
+
+<h5> Nieuwe Rubriek maken </h5>
+<label> Rubriek naam: </label>
+      <input type="text">
+      <label> Subrubrieken: </label>
+      <input type="text">
 
 
 
