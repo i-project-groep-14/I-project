4a1659604aee9decf13f2e8752df818862047fd7
memo043 <mehmet.batal@hotmail.com>
2019-05-15 15:06:14 +0200

    mail

--- Website/testmail.php
+++ Website/testmail.php
@@ -5,7 +5,7 @@ $ontvanger = 'spamvandanny2000@gmail.com';
 $onderwerp = 'Test';
 $tekst = "Line 1\r\nLine 2\r\nLine 3";
 $tekst = wordwrap($tekst, 70, "\r\n");
-$headers = 'From: eenmaailandermaal@.com' . "\r\n" .
+$headers = 'From: Noreply-EenmaalAndermaal@icasites.nl' . "\r\n" .
 'X-Mailer: PHP/' . phpversion();
 
 mail($ontvanger, $onderwerp, $tekst, $headers);
