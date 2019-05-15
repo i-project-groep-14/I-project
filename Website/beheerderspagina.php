36daf61ec7f49dde681a1c4162086a42b6ceda50
HendrikOtten28 <44369979+HendrikOtten28@users.noreply.github.com>
2019-05-15 11:21:31 +0200

    f

--- Website/beheerderspagina.php
+++ Website/beheerderspagina.php
@@ -4,15 +4,17 @@
       require_once 'aanroepingen/connectie.php';
       include_once 'aanroepingen/header.php';
     ?>
-    <div class="holy-grail-middle">
-<h3> Beheerders Pagina </h3>
-<h4> Beheren van de rubrieken </h4>
+<div class="holy-grail-middle">
+
+    <h3> Beheerders Pagina </h3>
+        <h4> Beheren van de rubrieken </h4>
 
 
 
 
 
 </div>
+
 <?php 
 include_once 'aanroepingen/footer.html';
 ?>
