1d54408f373993d28ed79f0cbf18b9de451911bb
HendrikOtten28 <44369979+HendrikOtten28@users.noreply.github.com>
2019-05-20 10:49:34 +0200

    Functioneel Ontwerp + Footer veranderd + contactpagina + FAQ

---
+++ Website/contact.php
@@ -0,0 +1,14 @@
+<?php
+      $config = ['pagina' => 'contact'];
+      require_once 'aanroepingen/connectie.php';
+      include_once 'aanroepingen/header.php';
+    ?>
+    
+    <div class="holy-grail-middle">
+    <h3> Contact </h3>
+    
+
+    </div>
+    <?php 
+      include_once 'aanroepingen/footer.html';
+    ?>
---
+++ Website/faq.php
@@ -0,0 +1,23 @@
+<?php
+  $config = ['pagina' => 'faq'];
+ require_once 'aanroepingen/connectie.php';
+ include_once 'aanroepingen/header.php'; 
+?>
+
+<div class="holy-grail-middle">
+<h3> FAQ'S </h3> 
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include_once 'aanroepingen/footer.html' ?>
