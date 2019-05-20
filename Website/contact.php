bc81a786822b58643bb1c98a764b9e904a1ea44c
HendrikOtten28 <44369979+HendrikOtten28@users.noreply.github.com>
2019-05-20 11:04:22 +0200

    Contact pagina + footer

--- Website/contact.php
+++ Website/contact.php
@@ -5,8 +5,38 @@
     ?>
     
     <div class="holy-grail-middle">
-    <h3> Contact </h3>
     
+    <section class="contact-us-section">
+    <h3> Contact </h3>
+  <div class="row medium-unstack">
+
+      <ul class="contact-us-list">
+        <li class="address">Arnhem, Ruitberglaan 26, Nederland</li>
+        <li class="email"><a href="mailto:">EenmaalAndermaal@gmail.com</a></li>
+        <li class="phone">
+          06 53467895
+        </li>
+      </ul>
+    </div>
+    <br>
+    <div class="columns contact-us-section-right">
+      <h4 class="contact-us-header">Stuur ons een bericht</h4>
+      <form class="contact-us-form">
+        <input type="text" placeholder="Volledige naam">
+        <input type="email" placeholder="Email">
+        <textarea name="message" id="" rows="12" placeholder="Type hier uw bericht"></textarea>
+        <div class="contact-us-form-actions">
+          <input type="submit" class="button" value="Versturen" />
+          <div>
+            <label for="FileUpload" class="button contact-us-file-button">Voeg bestand toe</label>
+            <input type="file" id="FileUpload" class="show-for-sr">
+          </div>
+        </div>
+      </form>
+    </div>
+</section>
+
+
 
     </div>
     <?php 
