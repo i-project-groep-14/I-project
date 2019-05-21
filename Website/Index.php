236b872aa546be83988618d05effe686a988c5ad
Danny <48557943+P00PF4C3@users.noreply.github.com>
2019-05-21 11:15:02 +0200

    Index tijd werkend

--- Website/Index.php
+++ Website/Index.php
@@ -10,23 +10,23 @@
         if(isset($_SESSION['login'])) {
                                             // verander in 0
           if($_SESSION['aantaleigenveilingen'] != 0) {
-            function dateDifference($date_1, $date_2, $differenceFormat = '%a') {
-              $datetime1 = date_create($date_1);
-              $datetime2 = date_create($date_2);
-              
-              $interval = date_diff($datetime1, $datetime2);
-              
-              return $interval->format($differenceFormat);
+            function timeDiff($firstTime,$lastTime){
+              $firstTime=strtotime($firstTime);
+              $lastTime=strtotime($lastTime);
+           
+              $timeDiff=$lastTime-$firstTime;
+           
+              return $timeDiff;
             }
 
-            function createHomepageItem($plek) {
+            function createHomepageItem($actueleplek) {
               global $dbh;
-              settype($plek, "int");
-              $volgendeplek = $plek+1;
+              settype($actueleplek, "int");
+              $volgendeplek = $actueleplek+1;
               
               $sql = "SELECT veilingGesloten FROM voorwerp 
                       WHERE verkoper like :gebruikersnaam
-                      ORDER BY titel OFFSET $plek ROWS FETCH NEXT $volgendeplek ROWS ONLY";
+                      ORDER BY titel OFFSET $actueleplek ROWS FETCH NEXT $volgendeplek ROWS ONLY";
               $query = $dbh->prepare($sql);
               $query -> execute(array(
                   ':gebruikersnaam' => $_SESSION['gebruikersnaam']
@@ -34,12 +34,12 @@
 
               $row = $query -> fetch();
 
-              global $test;
+              global $plek;
               
               if ($row['veilingGesloten'] == 'niet') {
                 $sql = "SELECT titel, verkoopprijs, looptijdeindeDag, looptijdeindeTijdstip, voorwerpnummer FROM voorwerp 
                         WHERE verkoper like :gebruikersnaam
-                        ORDER BY titel OFFSET $plek ROWS FETCH NEXT $volgendeplek ROWS ONLY";
+                        ORDER BY titel OFFSET $actueleplek ROWS FETCH NEXT $volgendeplek ROWS ONLY";
                 $query = $dbh->prepare($sql);
                 $query -> execute(array(
                     ':gebruikersnaam' => $_SESSION['gebruikersnaam']
@@ -51,11 +51,12 @@
                 $hoogstebod = $row['verkoopprijs'];
                 $voorwerpnummer = $row['voorwerpnummer'];
                 $looptijdeindeDag = $row['looptijdeindeDag'];
-                $looptijdeindeTijdstip = $row['looptijdeindeTijdstip'];
-                $actueledatum = date("Y-m-d");
-                $actueletijd = date("H-i-s");
-
-                $verschilInDagen = dateDifference($looptijdeindeDag, $actueledatum, "%d");
+                $looptijdeindeTijdstip = $row['looptijdeindeTijdstip'];$combinedDT = date('Y-m-d H:i:s', strtotime("$looptijdeindeDag $looptijdeindeTijdstip"));
+                $difference = timeDiff(date("Y-m-d H:i:s"),$combinedDT);
+                $years = abs(floor($difference / 31536000));
+                $days = abs(floor(($difference-($years * 31536000))/86400));
+                $hours = abs(floor(($difference-($years * 31536000)-($days * 86400))/3600));
+                // $mins = abs(floor(($difference-($years * 31536000)-($days * 86400)-($hours * 3600))/60));#floor($difference / 60);
 
                 $sql = "SELECT filenaam FROM bestand
                 WHERE voorwerp like :voorwerpnummer";
@@ -77,20 +78,14 @@
                   <img src='$afbeelding' alt='fiets'>
                   <h4>$titel</h4>
                   <p class='price'>€$hoogstebod</p>
-                  <p> <i class='fa fi-clock' style='font-size:24px'>&nbsp;</i>Sluit over: 
-                  ".$verschilInDagen.
-                  //"7
-                  "d
-                  ...
-                  u
-                  </p>
+                  <p> <i class='fa fi-clock' style='font-size:24px'>&nbsp;</i>Sluit over: "."$days"."d $hours"."u</p>
                   <a href='product.php' class='button ProductButton'>Bekijk Meer!</a>
                 </div>
                 ";
-                $test += 1;
+                $plek += 1;
               } else {
-                $test = $volgendeplek;
-                createHomepageItem($test);
+                $plek = $volgendeplek;
+                createHomepageItem($plek);
               }
             }
             $sql = "SELECT rol FROM gebruiker 
@@ -106,16 +101,16 @@
               <h3 class='HomePageTitel'>Uw veilingen</h3>
               <div class='ProductenContainer'>
               ";
-              $test = 0;
+              $plek = 0;
               for($i = 0; $i < 4; $i++) {
-                createHomepageItem($test);
+                createHomepageItem($plek);
               }
               echo "</div>";
             }
           }
         }
       ?>
-      
+
       <h3 class='HomePageTitel'>De populairste veilingen</h3>
       <div class='ProductenContainer'>
         <div class="card">
