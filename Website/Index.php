54de5d30868769eccbe9eef4d5bb8343f6e36bdc
Danny <48557943+P00PF4C3@users.noreply.github.com>
2019-05-21 13:04:13 +0200

    f
    f

--- Website/Index.php
+++ Website/Index.php
@@ -42,112 +42,65 @@
       <h3 class='HomePageTitel'>De populairste veilingen</h3>
       <div class='ProductenContainer'>
         <?php
-          $sql = "SELECT veilingGesloten FROM voorwerp 
-          WHERE verkoper like :gebruikersnaam
-          ORDER BY titel OFFSET $actueleplek ROWS FETCH NEXT $volgendeplek ROWS ONLY";
-          $query = $dbh->prepare($sql);
-          $query -> execute(array(
-              ':gebruikersnaam' => $_SESSION['gebruikersnaam']
-          ));
+          // $sql = "SELECT top 4 * FROM bod 
+          //         WHERE voorwerp like :voorwerpnummer";
+          // $query = $dbh->prepare($sql);
+          // $query -> execute(array(
+          //     ':voorwerpnummer' => ...
+          // ));
 
-          $row = $query -> fetch();
+          // $row = $query -> fetch();
 
-        createHomepageCard();
-        createHomepageCard();
-        createHomepageCard();
-        createHomepageCard();
+        // createHomepageCard();
+        // createHomepageCard();
+        // createHomepageCard();
+        // createHomepageCard();
         ?>
-        <!-- <div class="card">
-          <img src="images/fiets.jpg" alt="fiets">
-          <h4>Viking Fiets</h4>
-          <p class="price">€ 19.99</p>
-          <p> <i class='fa fi-clock' style='font-size:24px'>&nbsp;</i>Sluit over: 7d 12u</p>
-          <a href='product.php' class='button ProductButton'>Bekijk Meer!</a>
-        </div> 
-        <div class="card">
-          <img src="images/fiets.jpg" alt="fiets">
-          <h4>Viking Fiets</h4>
-          <p class="price">€ 19.99</p>
-          <p> <i class='fa fi-clock' style='font-size:24px'>&nbsp;</i>Sluit over: 7d 12u</p>
-          <a href='product.php' class='button ProductButton'>Bekijk Meer!</a>
-        </div>
-        <div class="card">
-          <img src="images/fiets.jpg" alt="fiets">
-          <h4>Viking Fiets</h4>
-          <p class="price">€ 19.99</p>
-          <p> <i class='fa fi-clock' style='font-size:24px'>&nbsp;</i>Sluit over: 7d 12u</p>
-          <a href='product.php' class='button ProductButton'>Bekijk Meer!</a>
-        </div>
-        <div class="card">
-          <img src="images/fiets.jpg" alt="fiets">
-          <h4>Viking Fiets</h4>
-          <p class="price">€ 19.99</p>
-          <p> <i class='fa fi-clock' style='font-size:24px'>&nbsp;</i>Sluit over: 7d 12u</p>
-          <a href='product.php' class='button ProductButton'>Bekijk Meer!</a>
-        </div> -->
       </div>
 
       <h3 class='HomePageTitel'>Loopt bijna af!</h3>
       <div class='ProductenContainer'>
-        <div class="card">
-          <img src="images/fiets.jpg" alt="fiets">
-          <h4>Viking Fiets</h4>
-          <p class="price">€ 19.99</p>
-          <p> <i class='fa fi-clock' style='font-size:24px'>&nbsp;</i>Sluit over: 7d 12u</p>
-          <a href='product.php' class='button ProductButton'>Bekijk Meer!</a>
-        </div> 
-        <div class="card">
-          <img src="images/fiets.jpg" alt="fiets">
-          <h4>Viking Fiets</h4>
-          <p class="price">€ 19.99</p>
-          <p> <i class='fa fi-clock' style='font-size:24px'>&nbsp;</i>Sluit over: 7d 12u</p>
-          <a href='product.php' class='button ProductButton'>Bekijk Meer!</a>
-        </div>
-        <div class="card">
-          <img src="images/fiets.jpg" alt="fiets">
-          <h4>Viking Fiets</h4>
-          <p class="price">€ 19.99</p>
-          <p> <i class='fa fi-clock' style='font-size:24px'>&nbsp;</i>Sluit over: 7d 12u</p>
-          <a href='product.php' class='button ProductButton'>Bekijk Meer!</a>
-        </div>
-        <div class="card">
-          <img src="images/fiets.jpg" alt="fiets">
-          <h4>Viking Fiets</h4>
-          <p class="price">€ 19.99</p>
-          <p> <i class='fa fi-clock' style='font-size:24px'>&nbsp;</i>Sluit over: 7d 12u</p>
-          <a href='product.php' class='button ProductButton'>Bekijk Meer!</a>
-        </div>
+        <?php
+        //   $sql = "SELECT voorwerpnummer, titel, verkoopprijs, looptijdeindeDag, looptijdeindeTijdstip FROM voorwerp 
+        //           WHERE looptijdeindeDag like :voorwerpnummer";
+        //   $query = $dbh->prepare($sql);
+        //   $query -> execute(array(
+        //       ':voorwerpnummer' => ...
+        //   ));
+
+        //   $row = $query -> fetch();
+
+        //   $titel = $row['titel'];
+        //   $hoogstebod = $row['verkoopprijs'];
+        //   $voorwerpnummer = $row['voorwerpnummer'];
+        //   $looptijdeindeDag = $row['looptijdeindeDag'];
+        //   $looptijdeindeTijdstip = $row['looptijdeindeTijdstip'];
+        //   $combinedDT = date('Y-m-d H:i:s', strtotime("$looptijdeindeDag $looptijdeindeTijdstip"));
+        //   $difference = timeDiff(date("Y-m-d H:i:s"),$combinedDT);
+        //   $years = abs(floor($difference / 31536000));
+        //   $days = abs(floor(($difference-($years * 31536000))/86400));
+        //   $hours = abs(floor(($difference-($years * 31536000)-($days * 86400))/3600));
+        //   $mins = abs(floor(($difference-($years * 31536000)-($days * 86400)-($hours * 3600))/60));
+        //   $secs = abs(floor(($difference-($years * 31536000)-($days * 86400)-($hours * 3600))-($mins * 3600)/60));
+
+        //   $sql = "SELECT filenaam FROM bestand 
+        //           WHERE voorwerp like :voorwerpnummer";
+        //   $query = $dbh->prepare($sql);
+        //   $query -> execute(array(
+        //       ':voorwerpnummer' => $voorwerpnummer
+        //   ));
+
+        //   $row = $query -> fetch();
+
+        // createHomepageCard($row['filenaam']);
+        // createHomepageCard();
+        // createHomepageCard();
+        // createHomepageCard();
+        ?>
       </div>
       <h3 class='HomePageTitel'>Nieuwe veilingen</h3>
       <div class='ProductenContainer'>
-        <div class="card">
-          <img src="images/fiets.jpg" alt="fiets">
-          <h4>Viking Fiets</h4>
-          <p class="price">€ 19.99</p>
-          <p> <i class='fa fi-clock' style='font-size:24px'>&nbsp;</i>Sluit over: 7d 12u</p>
-          <a href='product.php' class='button ProductButton'>Bekijk Meer!</a>
-        </div> 
-        <div class="card">
-          <img src="images/fiets.jpg" alt="fiets">
-          <h4>Viking Fiets</h4>
-          <p class="price">€ 19.99</p>
-          <p> <i class='fa fi-clock' style='font-size:24px'>&nbsp;</i>Sluit over: 7d 12u</p>
-          <a href='product.php' class='button ProductButton'>Bekijk Meer!</a>
-        </div>
-        <div class="card">
-          <img src="images/fiets.jpg" alt="fiets">
-          <h4>Viking Fiets</h4>
-          <p class="price">€ 19.99</p>
-          <p> <i class='fa fi-clock' style='font-size:24px'>&nbsp;</i>Sluit over: 7d 12u</p>
-          <a href='product.php' class='button ProductButton'>Bekijk Meer!</a>
-        </div>
-        <div class="card">
-          <img src="images/fiets.jpg" alt="fiets">
-          <h4>Viking Fiets</h4>
-          <p class="price">€ 19.99</p>
-          <p> <i class='fa fi-clock' style='font-size:24px'>&nbsp;</i>Sluit over: 7d 12u</p>
-          <a href='product.php' class='button ProductButton'>Bekijk Meer!</a>
-        </div>
+        
       </div>
     </div>
     
