cf7803bd32ee731bd157414701fbbe16f080bc1b
HendrikOtten28 <44369979+HendrikOtten28@users.noreply.github.com>
2019-05-22 12:52:32 +0200

    pop up bieden

--- Website/aanroepingen/timerfunctie.php
+++ Website/aanroepingen/timerfunctie.php
@@ -1,14 +1,14 @@
-
+<!-- Display the countdown timer in an element -->
+<p id="demo"></p>
 
 <script>
-function timer() {
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
 
 // Update the count down every 1 second
 var x = setInterval(function() {
 
-  // Get todays date and time
+  // Get today's date and time
   var now = new Date().getTime();
 
   // Find the distance between now and the count down date
@@ -30,5 +30,4 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
-}
 </script>
