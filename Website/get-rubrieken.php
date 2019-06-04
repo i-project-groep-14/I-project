825595cbb58a78a41122a58f6fd204c1eb923c29
ESChaos <43702613+ESChaos@users.noreply.github.com>
2019-06-04 13:56:41 +0200

    Rubrieken nav update javascript

--- Website/get-rubrieken.php
+++ Website/get-rubrieken.php
@@ -2,17 +2,20 @@
     require_once 'aanroepingen/connectie.php';
     if(isset($_GET['rubrieknummer'])){
         $rubrieknummer= $_GET['rubrieknummer'];
-        $sql = "SELECT * FROM rubriek WHERE rubriek = :rubrieknummer";
+        $sql = "SELECT *  FROM rubriek WHERE rubriek = :rubrieknummer";
         $query = $dbh->prepare($sql);
         $query -> execute(array(':rubrieknummer' => $rubrieknummer));
 
-        $data = array();
 
+        $data = array();
+        
         while($row = $query -> fetch()){
          array_push($data,$row);
          //echo json_encode($data);
         }
         echo json_encode($data);
+  
     }
+
 
 ?>
