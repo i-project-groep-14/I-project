dce291abfe7b9a4e92b6080b5dd826bc21b8aa44
HendrikOtten28 <44369979+HendrikOtten28@users.noreply.github.com>
2019-06-06 12:36:15 +0200

    Zoeken

--- Website/zoekresultaten.php
+++ Website/zoekresultaten.php
@@ -3,6 +3,26 @@
 
       require_once 'aanroepingen/connectie.php';
       include_once 'aanroepingen/header.php';
+
+      if (isset($_POST['zoeken'])) {
+        $zoekterm = $_POST['zoekwoord'];
+        $sql = "SELECT [rubriek op laagste niveau] as rubriek FROM [voorwerp in rubriek] r inner join voorwerp v on v.voorwerpnummer = r.voorwerp
+                WHERE v.titel like '%$zoekterm%'";
+        $query = $dbh->prepare($sql);
+        $query -> execute();
+
+        $plek = 0;
+        while ($row = $query -> fetch()) {  
+          $aantalVoorwerpen = selectAantalVoorwerpen($row['rubriek']);
+  
+          for($i = 0; $i < $aantalVoorwerpen; $i++) {
+            $plek = createVoorwerpInRubriekItem($plek, $row['rubriek']);
+          }
+        }
+
+        // echo $zoekterm;
+      }
+
     ?>
 
 <div class="holy-grail-middle">
