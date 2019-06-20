71b5952a1f6b291ceee5c5e4d6dbcc76993049b9
HendrikOtten28 <44369979+HendrikOtten28@users.noreply.github.com>
2019-06-20 10:40:54 +0200

    voor mehmet

---
+++ Database/Quicktime event.sql
@@ -0,0 +1,6 @@
+use iproject14
+
+insert into voorwerp values('Test', 'f', 1, 'PayPal', null, 'plaatsnaam7', 'land7', 0, GETDATE(), CONVERT(TIME(0),GETDATE()), null, null, 
+							'Beheerder', null, CONVERT(TIME,dateadd(second, 15, GETDATE())), 'niet', 60)
+
+use master
